6ec675763f58a5709cf5f624043a3daebe72cfe1
cangcang <cangcang_code@icloud.com>
2026-08-22 20:26:41 +0800

    add OPL license headers to source files

--- oakctl.sh
+++ oakctl.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+# Copyright (C) 2026 OPENOS-dev
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the OPENOS-PROJECT-LICENSE (OPL) v1.2.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# OPL for more details.
+#
+# You should have received a copy of the OPL along with this program.
+# If not, see <https://github.com/OPENOS-dev/OPL>.
+#
 # OAK 密钥管理工具 (oakctl)
 # 用途: 生成/解析 .oak 密钥文件 (openssl), 注册公钥到 /proc/oak/subjects。
 #
